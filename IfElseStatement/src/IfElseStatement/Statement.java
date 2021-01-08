f91f9f9ae6223a76fccd63ebc3b7924cde92cb49
Botir Musurmankulov <musurmankulov.botir@gmail.com>
2021-01-07 21:59:16 -0600

    commit changes!

--- IfElseStatement/src/IfElseStatement/Statement.java
+++ IfElseStatement/src/IfElseStatement/Statement.java
@@ -2,7 +2,7 @@ package IfElseStatement;
 
 public class Statement {
    
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		
 		String name = "John";
 		int age = 28;
