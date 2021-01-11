a58c5674c88a705cc68ef10c0cd0e4cb249239d1
Botir Musurmankulov <musurmankulov.botir@gmail.com>
2021-01-10 20:52:54 -0600

    commit

--- Test Automation/src/Automation/Test.java
+++ Test Automation/src/Automation/Test.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Test{
 	
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		
 		
 		System.setProperty("webdriver.chrome.driver", "C:/ChromeDriver/chromedriver.exe");
