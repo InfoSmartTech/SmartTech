562cde51f1874e5a72e9618ca78035d91c2491a9
Botir Musurmankulov <musurmankulov.botir@gmail.com>
2021-01-30 20:27:15 -0600

    develop ForLoop

---
+++ src/Loop/ForLoop.java
@@ -0,0 +1,24 @@
+package Loop;
+
+public class ForLoop {
+	
+	public static void main(String[] args) {
+		
+		int number = 100;
+		
+		for(int i = 0; i<number; i++) {
+			
+			if(i==51) {
+				//continue;
+				break;
+				
+			}else{
+				System.out.println(i);
+				
+			}			
+			
+		}
+		
+	}
+	
+}
